3ad9d8a13c29b51898d871d463525e6149756595
Olha Wysocky and Yulia Shidlovskaya <olha@>
2019-05-07 12:16:49 -0700

    add new and upvote tests

--- __tests__/post-list-reducer.test.js
+++ __tests__/post-list-reducer.test.js
@@ -1,7 +1,62 @@
 import postListReducer from "./../src/reducers/post-list-reducer";
+import Moment from "moment";
 
 describe("test postListReducer", () => {
+  let action;
+  let testPost = {
+    title: "Gentle doggo",
+    post: "Doggo was taught to be gentle when taking treats",
+    id: 0,
+    vote: 23,
+    timeCreated: Moment()
+  };
   test("returns empty object when not defined", () => {
     expect(postListReducer({}, { type: null })).toEqual({});
+  });
+
+  test("add new post", () => {
+    const { title, post, id, timeCreated } = testPost;
+    action = {
+      type: "NEW_POST",
+      title: title,
+      post: post,
+      id: id,
+      timeCreated: timeCreated
+    };
+    expect(postListReducer({}, action)).toEqual({
+      [id]: {
+        title: title,
+        post: post,
+        vote: 0,
+        id: id,
+        timeCreated: timeCreated
+      }
+    });
+  });
+
+  test("upvote post", () => {
+    const { title, post, id, timeCreated, vote } = testPost;
+    let existingPost = {
+      [id]: {
+        title: title,
+        post: post,
+        vote: vote,
+        id: id,
+        timeCreated: timeCreated
+      }
+    };
+
+    let newPost = {
+      [id]: {
+        title: title,
+        post: post,
+        vote: vote + 1,
+        id: id,
+        timeCreated: timeCreated
+      }
+    };
+
+    action = { type: "UPVOTE", id: id };
+    expect(postListReducer(existingPost, action)).toEqual(newPost);
   });
 });
--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,3 +1,24 @@
 export default (state = {}, action) => {
-  return state;
+  let newState;
+  switch (action.type) {
+    case "NEW_POST":
+      const { id, title, post, timeCreated } = action;
+      newState = Object.assign({}, state, {
+        [id]: {
+          id: id,
+          title: title,
+          post: post,
+          vote: 0,
+          timeCreated: timeCreated
+        }
+      });
+      return newState;
+    case "UPVOTE":
+      let newVote = state[action.id].vote + 1;
+      let newPost = Object.assign({}, state[action.id], { vote: newVote });
+      newState = Object.assign({}, state, { [action.id]: newPost });
+      return newState;
+    default:
+      return state;
+  }
 };
